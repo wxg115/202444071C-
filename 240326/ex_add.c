550d841e2f9924349d3facb2ca465a2252fa11f3
wxg115 <74805198+wxg115@users.noreply.github.com>
2024-04-02 11:24:04 +0900

    Add files via upload

---
+++ 240326/ex_add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int minus(int a, int b);
+int multi(int a, int b);
+int div(int a, int b);
+int main()
+{
+  int a, b;
+  int result=0;
+  scanf("%d %d", &a, &b);
+  result=add(a,b);
+  printf("%d + %d = %d\n",a,b,result);
+  result=minus(a,b);
+  printf("%d - %d = %d\n",a,b,result);
+  result=multi(a, b);
+  printf("%d * %d = %d\n",a,b,result);
+  result=div(a, b);
+  printf("%d / %d = %d",a,b,result);
+
+  return 0;
+}
+int add(int a, int b)
+{
+  return a+b;
+}
+int minus(int a, int b)
+{
+  return a-b;
+}
+int multi(int a, int b)
+{
+  return a*b;
+}
+int div(int a, int b)
+{
+  return a/b;
+}
